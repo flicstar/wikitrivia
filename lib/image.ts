39c8258f3bc0dfe2570d9dc818c584329a093749
flicstar <felicitybrand@gmail.com>
2025-12-03 17:30:48 +1100

    make fam images work

--- lib/image.ts
+++ lib/image.ts
@@ -1,4 +1,5 @@
 export function createWikimediaImage(image: string, width = 300): string {
+  if (image.startsWith("images/")) return "/" + image;
   return `https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/${encodeURIComponent(
     image
   )}&width=${width}`;
