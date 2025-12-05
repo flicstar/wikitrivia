12275962b6cff1d417f603ebde06372a65ce2603
flicstar <felicitybrand@gmail.com>
2025-12-05 17:03:49 +1100

    back to source

--- lib/items.ts
+++ lib/items.ts
@@ -20,7 +20,7 @@ export function getRandomItem(deck: Item[], played: Item[]): Item {
   const [fromYear, toYear] =
     periods[Math.floor(Math.random() * periods.length)];
   const avoidPeople = Math.random() > 0.5;
-  const candidates = deck.filter((candidate) => {
+  const candidates = source.filter((candidate) => {
     if (avoidPeople && candidate.instance_of.includes("human")) {
       return false;
     }
